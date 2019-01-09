969a43aaafd86e916bf7569c6a99ab54078dc18d
Glitch (hello-express) <none>
2019-01-09 09:34:24 +0000

    🐈☠️ Checkpoint ./server.js:2689143/337 ./views/index.html:2689143/67

--- server.js
+++ server.js
@@ -4,18 +4,25 @@
 // init project
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
+const cors = require('cors');
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
+app.use(bodyParser.json());
+app.use(cors());
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-
+app.post('/upload', upload.single('file'), (req, res, next) => {
+  res.send(req.file);
+});
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
